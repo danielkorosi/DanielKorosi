313121ce4dabdcce2d5989c33a27684fb7d41337
Daniel Korosi <daniel.korosi42@gmail.com>
2017-03-21 13:44:31 +0100

    py-exercise 07-11

---
+++ week2/day2/favorite-number.py
@@ -0,0 +1,2 @@
+favorite_number = 3
+print ("My favorite number is", favorite_number)
---
+++ week2/day2/seconds-in-a-day.py
@@ -0,0 +1,10 @@
+current_hours = 14
+current_minutes = 34
+current_seconds = 42
+day_sec = 60*60*12
+
+current_time_sec = current_hours*(60**2)+current_minutes*60+current_seconds
+remain_sec = day_sec % current_time_sec
+
+print (current_time_sec)
+print (remain_sec)
