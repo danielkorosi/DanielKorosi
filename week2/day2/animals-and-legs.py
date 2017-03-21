bd64a61eb8aa6f80ebf646f53c3b6bf360fdbb38
Daniel Korosi <daniel.korosi42@gmail.com>
2017-03-21 15:55:27 +0100

    ex16 animals-legs plus correction of mile=km

---
+++ week2/day2/animals-and-legs.py
@@ -0,0 +1,11 @@
+# Write a program that asks for two integers
+# The first represents the number of chickens the farmer has
+# The seconf represents the number of pigs the farmer has
+# It should print how many legs all the animals have
+
+print ("Dear farmer, please add the number of chickens you have:")
+ch = int(input())
+print ("And now please add the number of pigs you have:")
+p =  int(input())
+
+print ("The total number of legs all the animals have is: ", ch*2+p*4)
