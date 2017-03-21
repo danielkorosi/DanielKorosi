d7773f6c27873d889af2c027b4105a1aa2620c18
Daniel Korosi <daniel.korosi42@gmail.com>
2017-03-21 16:51:36 +0100

    ex20 biggernumber

---
+++ week2/day2/print-bigger.py
@@ -0,0 +1,10 @@
+# Write a program that asks for two numbers and prints the bigger one
+
+print ("Add two different numbers:")
+a = int(input())
+b = int(input())
+
+if a>b:
+    print (a)
+elif b>a:
+    print (b)
