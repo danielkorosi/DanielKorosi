6f81917e124b18938783e4493412469aad23dc31
Daniel Korosi <daniel.korosi42@gmail.com>
2017-03-22 14:39:12 +0100

    each element of a list is printed using a for loop

---
+++ week02/day03/print-all.py
@@ -0,0 +1,9 @@
+# - Create a variable named `af`
+#   with the following content: `[4, 5, 6, 7]`
+# - Print all the elements of `af`
+
+af = [4, 5, 6, 7]
+for a in af:
+    print(a)
+
+#each elements are printed, instead of printing them as a list with print(af)
