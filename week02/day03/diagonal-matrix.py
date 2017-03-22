da0bac09ba7fd40267df94150ab4a28f26234e10
Daniel Korosi <daniel.korosi42@gmail.com>
2017-03-22 17:45:22 +0100

    matrix first try

---
+++ week02/day03/diagonal-matrix.py
@@ -0,0 +1,17 @@
+# - Create (dynamically) a two dimensional list
+#   with the following matrix. Use a loop!
+#
+#   1 0 0 0
+#   0 1 0 0
+#   0 0 1 0
+#   0 0 0 1
+#
+# - Print this two dimensional list to the output
+
+matrix = [[ 0 for i in range(4)] for j in range(4)]
+for i in range(4):
+    for j in range(4):
+        if i == j:
+            matrix[i][j] = 1
+print(matrix)
+#matrix[0][0]=1
