ff0fd00e3393da8bfa62c101e882bd63da3fdbb1
Daniel Korosi <daniel.korosi42@gmail.com>
2017-03-22 11:55:26 +0100

    summing specific elements from a list

---
+++ week02/day03/sum-elements.py
@@ -0,0 +1,7 @@
+# - Create a variable named `r`
+#   with the following content: `[54, 23, 66, 12]`
+# - Print the sum of the second and the third element
+
+r = [54, 23, 66, 12]
+z = r[1:3]
+print(sum(z))
