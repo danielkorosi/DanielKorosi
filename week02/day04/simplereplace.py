e45ec862ae9a42b5dcbba4c50f8ff71127defe10
Daniel Korosi <daniel.korosi42@gmail.com>
2017-03-23 11:54:35 +0100

    replace long solution with string slicing

---
+++ week02/day04/simplereplace.py
@@ -0,0 +1,15 @@
+# I would like to replace "dishwasher" with "galaxy" in this example
+# Please fix it for me!
+# Expected ouput: In a galaxy far far away
+
+example = "In a dishwasher far far away"
+
+g = example[5:16]
+g = "galaxy"
+example ="In a "+ g +" far far away"
+print(example)
+
+
+
+#example = example.replace("dishwasher", "galaxy")
+#print(example)
