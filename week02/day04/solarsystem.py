292232f05c79a2e75a4203fca183744e9a8b4e61
Daniel Korosi <daniel.korosi42@gmail.com>
2017-03-23 13:11:25 +0100

    solarsystem short solution with built-in insert  funciton

---
+++ week02/day04/solarsystem.py
@@ -0,0 +1,9 @@
+# Saturn is missing from the planetList
+# Insert it into the correct position
+
+planetList = ["Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"]
+
+planetList.insert(5, "Saturn")
+print(planetList)
+
+#short solution woth built-in function
