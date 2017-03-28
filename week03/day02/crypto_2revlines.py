21cbde4448f5d162db3506b07029cea6a252878b
Daniel Korosi <daniel.korosi42@gmail.com>
2017-03-28 12:02:10 +0200

    reversing a text by line

---
+++ week03/day02/crypto_2revlines.py
@@ -0,0 +1,11 @@
+# Create a method that decrypts texts/reversed_zen_lines.txt
+def decrypt(file_name):
+    dcr = open(file_name, 'r')
+    text = dcr.readlines()
+    reversed_by_line = ''
+    for i in text:
+        i = i[::-1]
+        reversed_by_line += i
+    return reversed_by_line
+    
+print(decrypt('reversed-lines.txt'))
