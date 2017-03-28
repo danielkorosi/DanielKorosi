17f2f6a2b55658fa32570a5244c470ee43eff61a
Daniel Korosi <daniel.korosi42@gmail.com>
2017-03-28 16:54:31 +0200

    encoded lines halfway solution, accounting for correct order and spaces. line breaks to be added

---
+++ week03/day02/crypto_4encoded.py
@@ -0,0 +1,15 @@
+# Create a method that decrypts texts/encoded-lines.txt
+def decrypt(file_name):
+    dcr = open(file_name, 'r')
+    text = dcr.read()
+    int_version = ''
+    for i in text:
+        if i == ' ':
+            int_version += i
+        else:
+            i = ord(i)-1
+            i = chr(i)
+            int_version += i
+    return int_version
+
+print(decrypt('encoded-lines.txt'))
