5d143f4cf7134dad5e1886f0193d8bf1012ba491
Daniel Korosi <daniel.korosi42@gmail.com>
2017-03-29 17:29:32 +0200

    function drawing lines to the center shorter solution

--- week03/day03/function-to-center.py
+++ week03/day03/function-to-center.py
@@ -14,16 +14,7 @@ def centerline(x, y):
         line = canvas.create_line(x, y, 150, 150)
 
 for i in range(0, 301, 20):
-    centerline(0, i)
-    centerline(300, i)
-    centerline(i, 0)
-    centerline(i, 300)
-
-root.mainloop()
-
-    #if i > 15:
-    #    i = 0
-    #for j in range(0, 300, 20):
-    #    centerline(i, j)
-
+    for j in range(0, 301, 300):
+        centerline(i, j)
+        centerline(j, i)
 root.mainloop()
