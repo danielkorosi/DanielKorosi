dea05a2d049aae44f35329689b7e73446d5bf5bc
Daniel Korosi <daniel.korosi42@gmail.com>
2017-03-30 14:20:41 +0200

    recursive adding function with different values for odd and even members

---
+++ week03/day04/bunny2.py
@@ -0,0 +1,13 @@
+# We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+# (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+# have 3 ears, because they each have a raised foot. Recursively return the
+# number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+def ears(n):
+    if n == 1:
+        return 2
+    elif n % 2 == 0:
+        return 3 + ears(n-1)
+    else:
+        return 2 + ears(n-1)
+print(ears(8))
