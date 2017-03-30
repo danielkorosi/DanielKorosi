76bbb9da989b84564d9821526b957df23fd80c9c
Daniel Korosi <daniel.korosi42@gmail.com>
2017-03-30 10:54:20 +0200

    recursive numberadding function

---
+++ week03/day04/numberadder.py
@@ -0,0 +1,8 @@
+# Write a recursive function that takes one parameter: n and adds numbers from 1 to n
+
+def add(n):
+    if n == 1:
+        return n
+    else:
+        return n + add(n-1)
+print(add(5))
