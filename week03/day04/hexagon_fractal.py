67a5a6c6534330609f2a002aae1640796379eebc
Daniel Korosi <daniel.korosi42@gmail.com>
2017-03-31 10:36:22 +0200

    hexagon internal stage

---
+++ week03/day04/hexagon_fractal.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='600', height='600',)
+canvas.pack()
+def hexagon(x1, y1, size):
+    canvas.create_polygon(x1, y1, x1+size/4, y1-size/2, x1+size*3/4, y1-size/2, x1+size, y1, x1+size*3/4, y1+size/2, x1+size/4, y1+size/2, fill='', outline = 'black')
+
+    if size > 5:
+        hexagon(x1+size/8, y1-size/4, size/2)
+        hexagon(x1+size/2, y1, size/2)
+        hexagon(x1+size/8, y1+size/4, size/2)
+
+hexagon(20, 250, 300)
+root.mainloop()
