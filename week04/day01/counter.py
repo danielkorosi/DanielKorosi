7b061b30f7d10e9fd7f9598565c70b710f725da3
Daniel Korosi <daniel.korosi42@gmail.com>
2017-04-03 15:35:16 +0200

    counter with add, get and reset methods

---
+++ week04/day01/counter.py
@@ -0,0 +1,23 @@
+class Counter:
+
+    def __init__(self, value = 0):
+        self.value = int(value)
+        self.reset_value = int(value)
+
+    def add(self, number = 1):
+        self.value += number
+
+    def get(self):
+        return self.value
+
+    def reset(self):
+        self.value = self.reset_value
+
+
+n1 = Counter(2)
+n1.add(20)
+print(n1.value)
+n1.get()
+print(n1.value)
+n1.reset()
+print(n1.value)
