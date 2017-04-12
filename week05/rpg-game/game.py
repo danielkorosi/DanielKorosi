adf24e2f0f91bc055f16d889b94647f9ca8eaadb
Daniel Korosi <daniel.korosi42@gmail.com>
2017-04-12 14:30:01 +0200

    map with other logic, parent class for objects, map class, same problem again with opening images

--- week05/rpg-game/game.py
+++ week05/rpg-game/game.py
@@ -1,66 +1,58 @@
 from tkinter import*
 from PIL import Image, ImageTk
 
-root = Tk()
-canvas = Canvas(root, width=720, height=720)
-#image1 = Image.open('floor.png')
-#floor = ImageTk.PhotoImage(image1)
-#canvas.bind("<KeyPress>", move_hero)
-#canvas.pack()
-class Tile():
+class Objects():
+    def __init__(self, background=''):
+        self.background = background
+    def draw_object(self, x, y):
+        self.opened_image = Image.open(self.background)
+        self.image = ImageTk.PhotoImage(self.opened_image)
+        canvas.create_image(x, y, image = self.image)
+
+class Wall(Objects):
+    def __init__(self, background = 'wall.png'):
+        super().__init__(background)
+
+class Floor(Objects):
+    def __init__(self, background = 'floor.png'):
+        super().__init__(background)
+
+class Map():
     def __init__(self):
-        self.x = 36
-        self.y = 36
-        self.size = 72
-        self.img1 = Image.open('floor.png')
-        self.floor = ImageTk.PhotoImage(self.img1)
-        self.img2 = Image.open('wall.png')
-        self.wall = ImageTk.PhotoImage(self.img2)
+        root = Tk()
+        canvas = Canvas(root, width=720, height=720)
+        canvas.pack()
+        root.mainloop()
+        self.floor = Floor()
+        self.wall = Wall()
+        self.sketch_map()
+    def sketch_map(self):
+        self.map_template = [
+        [0, 0 , 0 , 1 , 0 , 1 , 0 , 0 , 0 , 0],
+        [0, 0 , 0 , 1 , 0 , 1 , 0 , 0 , 0 , 0],
+        [0, 1 , 1 , 1 , 0 , 1 , 0 , 1 , 1 , 0],
+        [0, 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0],
+        [1, 1 , 0 , 0 , 0 , 1 , 0 , 0 , 1 , 0],
+        [0, 1 , 0 , 1 , 0 , 0 , 0 , 0 , 1 , 0],
+        [0, 1 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0],
+        [0, 0 , 0 , 0 , 0 , 1 , 1 , 0 , 1 , 0],
+        [0, 1 , 1 , 1 , 0 , 0 , 0 , 0 , 1 , 0],
+        [0, 0 , 0 , 1 , 0 , 1 , 1 , 0 , 0 , 0]
+        ]
+        return self.map_template
 
-    def map(self):
-        for x in range(10):
-            for y in range(10):
-                self.floors = canvas.create_image(self.x+self.size*x, self.y+self.size*y, image=self.floor)
+    def draw_map(self):
+        for y in range(len(self.map_template)):
+            for x in range(len(self.map_template[y])):
+                if self.map_template[x][y] == 1:
+                    self.wall.draw_object(y*72,x*72)
+                if self.map_template[x][y] == 0:
+                    self.floor.draw_object(x*72,y*72)
+#class Hero(Objects):
+    #super().__init__(background):
+    #    self.x = 0
+    #    self.y = 0
+    #    self.create_object(self.x,self.y)
 
-        self.walls = [[3, 5], [3,5,7,8], [1,2,3,5,7,8], [5], [0,1,2,3,5,7,8], [1,3,8], [1,3,5,6,8], [5,6,8], [1,2,3,8], [3,5,6,8]]
-
-        for i in range(len(self.walls)):
-            for j in self.walls[i]:
-                canvas.create_image(self.x+self.size*j, self.y+self.size*i, image=self.wall)
-class Hero():
-    def __init__(self):
-        self.x = 36
-        self.y = 36
-        self.down = Image.open('hero-down.png')
-        self.img_down = ImageTk.PhotoImage(self.down)
-        self.up = Image.open('hero-up.png')
-        self.img_up = ImageTk.PhotoImage(self.up)
-        self.right = Image.open('hero-right.png')
-        self.img_right = ImageTk.PhotoImage(self.right)
-        self.left = Image.open('hero-left.png')
-        self.img_left = ImageTk.PhotoImage(self.left)
-
-
-    def draw_hero(self):
-        canvas.create_image(self.x, self.y, image=self.img_down)
-
-    def move_hero(self, e):
-        if e.keycode == 8320768:
-            hero.y = hero.y - 72
-        elif e.keycode == 8255233:
-            hero.y = hero.y + 72
-        elif e.keycode == 8189699:
-            hero.x = hero.x + 72
-        elif e.keycode == 8124162:
-            hero.x = hero.x - 72
-        hero.draw()
-
-canvas.bind("<KeyPress>", move_hero)
-canvas.pack()
-canvas.focus_set()
-tile = Tile()
-tile.map()
-hero = Hero()
-hero.draw_hero()
-
-root.mainloop()
+map = Map()
+map.draw_map()
