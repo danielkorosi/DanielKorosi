973a4607970defa89ed02bc108e7527802a2fc88
Daniel Korosi <daniel.korosi42@gmail.com>
2017-05-02 15:28:55 +0200

    repeated string with for loop

---
+++ week07/day02/i-wont-cheat-on-the-exams.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+for (var j = 1; j <= 10; j+=1) {
+  console.log('I won\'t cheat on the exam!');
+}
