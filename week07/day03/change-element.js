375fd36611445ee43e1a491b8247ee62566fed30
Daniel Korosi <daniel.korosi42@gmail.com>
2017-05-03 13:47:10 +0200

    changing an element of an array using .map

---
+++ week07/day03/change-element.js
@@ -0,0 +1,16 @@
+'use strict';
+// - Create an array named `s` with the following content: `[1, 2, 3, 8, 5, 6]`
+// - Change the 8 to 4 with the `.map` method
+// - Print the fourth element as a test
+
+var s = [1, 2, 3, 8, 5, 6];
+
+var sNew = s.map(function(value) {
+  if (value === 8) {
+    return 4;
+  } else {
+    return value;
+  }
+})
+
+console.log(sNew[3]);
