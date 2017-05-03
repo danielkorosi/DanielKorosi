d660ede889d9e639c62825799aa1464aeede76dd
Daniel Korosi <daniel.korosi42@gmail.com>
2017-05-03 14:06:44 +0200

    adding a letter to all array elements with forEach

---
+++ week07/day03/append-a-2.js
@@ -0,0 +1,12 @@
+'use strict';
+// - Create a variable named `nimals`
+//   with the following content: `["kuty", "macs", "cic"]`
+// - Add all elements an `"a"` at the end
+// - try to use built in functions instead of loops
+
+var nimals = ["kuty", "macs", "cic"];
+
+nimals.forEach(function(element){
+  element += 'a'
+  console.log(element);
+})
