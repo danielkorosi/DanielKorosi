36f0e37e6fc76118ed02abc862cf604c04c2707c
Daniel Korosi <daniel.korosi42@gmail.com>
2017-05-03 14:59:01 +0200

    matrix with only yeros created

---
+++ week07/day03/diagonal-matrix.js
@@ -0,0 +1,25 @@
+'use strict';
+// - Create (dynamically*) a two dimensional list
+//   with the following matrix**. Use a loop!
+//
+//   0 0 0 1
+//   0 0 1 0
+//   0 1 0 0
+//   1 0 0 0
+//
+// - Print this two dimensional list to the console
+//
+// * size should depend on a variable
+// ** Relax, a matrix is just like an array
+
+var matrix = []
+
+for (var i = 0; i < 4; i++) {
+  matrix[i] = [];
+  for (var j = 0; j<4; j++) {
+    matrix[i][j] = 0;
+  }
+}
+
+
+console.log(matrix);
