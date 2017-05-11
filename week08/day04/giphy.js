ae6f0849f7fc4190f152bc16f7ced8fe86655e6b
Daniel Korosi <daniel.korosi42@gmail.com>
2017-05-11 14:23:12 +0200

    giphy: gifs are played when clicked, worked only after everything was put into one function

--- week08/day04/giphy.js
+++ week08/day04/giphy.js
@@ -1,16 +1,46 @@
-let fox = new XMLHttpRequest();
+var fox = new XMLHttpRequest();
 fox.open('GET', 'http://api.giphy.com/v1/gifs/search?q=fox&api_key=dc6zaTOxFJmzC', true);
 fox.send();
+//fox.onreadystatechange = getState;
+
+ function getState() {
+   console.log(fox);
+}
 
 var pictures = document.querySelector('div');
 
 fox.onreadystatechange = function () {
  if (fox.readyState === 4 && fox.status === 200) {
     var resp = JSON.parse(fox.response);
-    for (var i = 0; i < 16; i++) {
+    console.log(resp);
+
+    resp.data.forEach(function(el) {
       var onePic = document.createElement('img');
-      onePic.setAttribute('src', resp.data[i].images.original_still.url);
-      pictures.appendChild(onePic)
-    }
+      onePic.setAttribute('src', el.images.original_still.url);
+      pictures.appendChild(onePic);
+      onePic.addEventListener('click', function() {
+        onePic.setAttribute('src', el.images.original.url);
+      })
+    })
   }
-}
+};
+
+
+/*function display(x) {
+  for (var i = 0; i < 16; i++) {
+    var onePic = document.createElement('img');
+    onePic.setAttribute('src', x.data[i].images.original_still.url);
+    pictures.appendChild(onePic);
+    onePic.addEventListener('click', function() {
+      onePic.setAttribute('src', x.data[i].images.original.url);
+    });
+  }
+};
+
+function startGif(z) {
+  //for (var j = 0; j < 16; j++) {
+    //var onePic = document.createElement('img');
+    z.setAttribute('src', z.data[i].images.downsized_medium.url);
+    //pictures.appendChild(y)
+  //}
+}*/
