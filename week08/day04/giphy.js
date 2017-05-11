918e367ca3b77a79ccf10d3f4141d1dc04cf382f
Daniel Korosi <daniel.korosi42@gmail.com>
2017-05-11 14:59:49 +0200

    giphy: applying the funciton to only the required first 16 elements

--- week08/day04/giphy.js
+++ week08/day04/giphy.js
@@ -8,7 +8,7 @@ fox.onreadystatechange = function () {
  if (fox.readyState === 4 && fox.status === 200) {
     var resp = JSON.parse(fox.response);
     console.log(resp);
-    var picData = resp.data;
+    var picData = resp.data.splice(0, 16);
     picData.forEach(function(el) {
       var onePic = document.createElement('img');
       onePic.setAttribute('src', el.images.original_still.url);
@@ -25,24 +25,6 @@ fox.onreadystatechange = function () {
   onePic.setAttribute('src', resp.data[i].images.original_still.url);
   pictures.appendChild(onePic);
   onePic.addEventListener('click', function() {
-    onePic.setAttribute('src', x.data[i].images.original.url);*/
-
-
-/*function display(x) {
-  for (var i = 0; i < 16; i++) {
-    var onePic = document.createElement('img');
-    onePic.setAttribute('src', x.data[i].images.original_still.url);
-    pictures.appendChild(onePic);
-    onePic.addEventListener('click', function() {
-      onePic.setAttribute('src', x.data[i].images.original.url);
-    });
-  }
-};
-
-function startGif(z) {
-  //for (var j = 0; j < 16; j++) {
-    //var onePic = document.createElement('img');
-    z.setAttribute('src', z.data[i].images.downsized_medium.url);
-    //pictures.appendChild(y)
-  //}
-}*/
+    onePic.setAttribute('src', resp.data[i].images.original.url);
+    })
+  }*/
