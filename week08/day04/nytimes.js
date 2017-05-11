e9dfda4ed69116deeb839bc4c52aac07c884bb40
Daniel Korosi <daniel.korosi42@gmail.com>
2017-05-11 16:35:43 +0200

    nytimes: retrieving data from new york times api, and creating an ordered list from them in html

---
+++ week08/day04/nytimes.js
@@ -0,0 +1,32 @@
+var request = new XMLHttpRequest();
+
+request.open('GET', 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=apollo+11&api_key=0e316cc3d899414ab623037473ae6466', true);
+request.send(null);
+
+var body = document.querySelector('body');
+
+request.onreadystatechange = function () {
+ if (request.readyState === 4 && request.status === 200) {
+    var resp = JSON.parse(request.response);
+    console.log(resp);
+
+    var docData = resp.response.docs;
+
+    docData.forEach(function (el) {
+      var details = document.createElement('ol');
+      body.appendChild(details);
+
+      var headline = document.createElement('li');
+      headline.textContent = el.headline.main;
+      details.appendChild(headline);
+
+      var snippet = document.createElement('li');
+      snippet.textContent = el.snippet;
+      details.appendChild(snippet);
+
+      var pubDate = document.createElement('li');
+      pubDate.textContent = el.pub_date;
+      details.appendChild(pubDate);
+    })
+  }
+}
