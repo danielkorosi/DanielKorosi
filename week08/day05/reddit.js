40f273a137aa89ef6de4a228feede484f3ece721
Daniel Korosi <daniel.korosi42@gmail.com>
2017-05-12 15:06:25 +0200

    reddit: get method restructured with callback function

--- week08/day05/reddit.js
+++ week08/day05/reddit.js
@@ -1,88 +1,96 @@
 'use strict';
-var request = new XMLHttpRequest();
 
-request.open('GET', 'https://time-radish.glitch.me/posts', true);
-request.setRequestHeader('Accept', 'application/json')
-request.send();
+function getData(callback) {
+  var request = new XMLHttpRequest();
 
+  request.open('GET', 'http://10.27.99.171:8080/posts', true);
+  request.setRequestHeader('Accept', 'application/json')
+  request.send();
 
-request.onreadystatechange = function () {
- if (request.readyState === 4 && request.status === 200) {
-    var resp = JSON.parse(request.response);
-    console.log(resp);
+  request.onreadystatechange = function () {
+   if (request.readyState === 4 && request.status === 200) {
+      var resp = JSON.parse(request.response);
+      //console.log(resp);
 
-    var docData = resp.posts;
+      var docData = resp.posts;
+      callback(docData)
+    }
+  }
+}
 
-    docData.forEach(function(el) {
-      var body = document.querySelector('body');
-      var entirePost = document.createElement('article');
-      body.appendChild(entirePost);
-      entirePost.className = 'post';
+function createPost(docData) {
+  docData.forEach(function(el) {
+    var body = document.querySelector('body');
+    var entirePost = document.createElement('article');
+    body.appendChild(entirePost);
+    entirePost.className = 'post';
 
-      var rank = document.createElement('div');
-      rank.className = 'rank';
-      rank.innerHTML = el.id+1;
-      entirePost.appendChild(rank);
-
-
-      var voting = document.createElement('div');
-      voting.className = 'voting';
-      entirePost.appendChild(voting);
-
-      var upVote = document.createElement('img');
-      upVote.className = 'up-vote';
-      upVote.setAttribute('src', 'upvote.png')
-      voting.appendChild(upVote);
-
-      var voteNumber = document.createElement('div');
-      voteNumber.className = 'vote-number';
-      voteNumber.innerHTML = el.score;
-      voting.appendChild(voteNumber);
-
-      var downVote = document.createElement('img');
-      downVote.className = 'down-vote';
-      downVote.setAttribute('src', 'downvote.png')
-      voting.appendChild(downVote);
+    var rank = document.createElement('div');
+    rank.className = 'rank';
+    rank.innerHTML = el.id;
+    entirePost.appendChild(rank);
 
 
-      var mainInfo = document.createElement('main');
-      mainInfo.className = 'main-info';
-      entirePost.appendChild(mainInfo);
+    var voting = document.createElement('div');
+    voting.className = 'voting';
+    entirePost.appendChild(voting);
 
-      var title = document.createElement('div');
-      title.className = 'title';
-      title.innerHTML = el.title;
-      mainInfo.appendChild(title);
+    var upVote = document.createElement('img');
+    upVote.className = 'up-vote';
+    upVote.setAttribute('src', 'upvote.png')
+    voting.appendChild(upVote);
 
+    var voteNumber = document.createElement('div');
+    voteNumber.className = 'vote-number';
+    voteNumber.innerHTML = el.score;
+    voting.appendChild(voteNumber);
 
-      var details = document.createElement('div');
-      details.className = 'details';
-      mainInfo.appendChild(details);
-
-      var time = document.createElement('div');
-      time.className = 'time';
-      time.innerHTML = 'here goes the time';
-      details.appendChild(time);
-
-      var author = document.createElement('div');
-      author.className = 'author';
-      author.innerHTML = 'here goes the author name'
-      details.appendChild(author);
+    var downVote = document.createElement('img');
+    downVote.className = 'down-vote';
+    downVote.setAttribute('src', 'downvote.png')
+    voting.appendChild(downVote);
 
 
-      var edit = document.createElement('div');
-      edit.className = 'edit';
-      mainInfo.appendChild(edit);
+    var mainInfo = document.createElement('main');
+    mainInfo.className = 'main-info';
+    entirePost.appendChild(mainInfo);
 
-      var modify = document.createElement('div');
-      modify.className = 'modify';
-      modify.innerHTML = 'modify';
-      edit.appendChild(modify);
+    var title = document.createElement('div');
+    title.className = 'title';
+    title.innerHTML = el.title;
+    mainInfo.appendChild(title);
 
-      var remove = document.createElement('div');
-      remove.className = 'remove';
-      remove.innerHTML = 'remove';
-      edit.appendChild(remove);
+
+    var details = document.createElement('div');
+    details.className = 'details';
+    mainInfo.appendChild(details);
+
+    var time = document.createElement('div');
+    time.className = 'time';
+    time.innerHTML = 'here goes the time';
+    details.appendChild(time);
+
+    var author = document.createElement('div');
+    author.className = 'author';
+    author.innerHTML = 'here goes the author name'
+    details.appendChild(author);
+
+
+    var edit = document.createElement('div');
+    edit.className = 'edit';
+    mainInfo.appendChild(edit);
+
+    var modify = document.createElement('button');
+    modify.className = 'modify';
+    modify.innerHTML = 'modify';
+    edit.appendChild(modify);
+
+    var remove = document.createElement('button');
+    remove.className = 'remove';
+    remove.innerHTML = 'remove';
+    edit.appendChild(remove);
+
   })
 }
-}
+
+getData(createPost);
