58c485e7a3303e632277f9b453cee592602fd911
Daniel Korosi <daniel.korosi42@gmail.com>
2017-05-15 14:36:09 +0200

    anagram function created

---
+++ week09/day01/anagram.js
@@ -0,0 +1,12 @@
+//Write a function, that takes two strings and returns a boolean value based on if the two strings are Anagramms or not.
+//Create a test for that.
+
+function Anagram(word1, word2) {
+  if (word1.split('').sort().join === word2.split('').sort().join) {
+    return true;
+} else {
+    return false;
+}
+}
+
+console.log(Anagram('asdf', 'dsfa'));
