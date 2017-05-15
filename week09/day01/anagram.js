b9d0e85edb72dcac243b707b2d18ddc6fed9f18f
Daniel Korosi <daniel.korosi42@gmail.com>
2017-05-15 14:41:31 +0200

    anagram function corrected

--- week09/day01/anagram.js
+++ week09/day01/anagram.js
@@ -2,11 +2,11 @@
 //Create a test for that.
 
 function Anagram(word1, word2) {
-  if (word1.split('').sort().join === word2.split('').sort().join) {
+  if (word1.split('').sort().join() === word2.split('').sort().join()) {
     return true;
-} else {
-    return false;
-}
+  } else {
+    return false
+  }
 }
 
 console.log(Anagram('asdf', 'dsfa'));
