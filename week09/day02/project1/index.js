8d85553550ca919c9ffbed4a8834a88264529ab6
Daniel Korosi <daniel.korosi42@gmail.com>
2017-05-16 13:18:48 +0200

    frontend done

---
+++ week09/day02/project1/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+
+});
+app.use('/assets', express.static('assets'))
+
+app.listen(3000);
