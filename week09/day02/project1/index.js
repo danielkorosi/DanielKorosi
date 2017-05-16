6b26ed10cb41cca9314cd39495e44adc23810419
Daniel Korosi <daniel.korosi42@gmail.com>
2017-05-16 13:51:26 +0200

    doubling: condition added

--- week09/day02/project1/index.js
+++ week09/day02/project1/index.js
@@ -12,10 +12,20 @@ app.use('/assets', express.static('assets'))
 
 app.get('/doubling', function(req, res) {
   var input = req.query.input;
+  if (input) {
+    res.send({
+      received: input,
+      result: input*2,
+  })
+} else {
   res.send({
-    "received": input,
-  "result": input*2,
-})
+    error: "Please provide an input!"
+  })
+}
 });
+
+//app.get('/doubling', function(req, res) {
+
+//})
 
 app.listen(8080);
