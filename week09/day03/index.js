a3b24bb243bef427624759dc9b5da8022f936b99
Daniel Korosi <daniel.korosi42@gmail.com>
2017-05-18 13:38:07 +0200

    bookstore second part done, listing from multiple tables

--- week09/day03/index.js
+++ week09/day03/index.js
@@ -19,15 +19,16 @@ conn.connect(function(err){
   console.log("Connection established");
 });
 
+//bookstore first part: list book titles
 const country = 'USA';
-const query = 'SELECT book_name FROM book_mast';
+const queryTitles = 'SELECT book_name FROM book_mast';
 
 var list = '<ul>';
 var li_open = '<li>';
 var li_close = '</li>';
 
-app.get('/', function get(req, res){
-  conn.query(query, function(err,rows){
+app.get('/titles', function get(req, res){
+  conn.query(queryTitles, function(err,rows){
     if (err){
       console.log("para van", err);
     } else {
@@ -38,6 +39,27 @@ app.get('/', function get(req, res){
       list += '</ul>';
       res.send(list);
       console.log(list);
+    }
+  });
+});
+
+//bookstore second part: all books with full data
+const queryAllBooks = 'SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM author LEFT JOIN book_mast ON author.aut_id = book_mast.aut_id LEFT JOIN category ON book_mast.cate_id = category.cate_id LEFT JOIN newpublisher ON book_mast.pub_id = newpublisher.pub_id';
+
+var table = '<table>';
+
+app.get('/allbooks', function get(req, res){
+  conn.query(queryAllBooks, function(err,rows){
+    if (err){
+      console.log("para van", err);
+    } else {
+      console.log("Data received from Db:\n");
+      console.log(rows);
+      rows.forEach(function(el) {
+        table += '<tr>' + '<th>'+ el.book_name + '</th>' + '<th>' + el.aut_name + '</th>' + '<th>' + el.cate_descrip + '</th>' + '<th>' + el.pub_name + '</th>' + '<th>' + el.book_price + '</th>' + '</tr>';
+      })
+      table += '</table>';
+      res.send(table);
     }
   });
 });
