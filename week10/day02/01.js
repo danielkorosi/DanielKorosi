78470313ad4ce4e31a5cfe67551b6924656abd71
Daniel Korosi <daniel.korosi42@gmail.com>
2017-05-23 13:14:52 +0200

    animal constructor + method added to its prototype

--- week10/day02/01.js
+++ week10/day02/01.js
@@ -4,6 +4,17 @@
 // it should take one parameter: what the animal says
 // Every animal should have a method called say() that prints what the animal says
 
+function Animal(sound) {
+    this.sound = sound;
+}
+Animal.prototype.say = function() {
+  console.log(this.sound);
+}
+
+var cow = new Animal('buuuu')
+cow.say()
+
+
 function Animal1(sound) {
   this.sound = sound;
   this.say1 = function() {
@@ -13,6 +24,7 @@ function Animal1(sound) {
 
 var cat = new Animal1('meeuw');
 cat.say1();
+
 
 class Animal2 {
   constructor(sound) {
