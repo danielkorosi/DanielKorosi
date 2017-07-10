824d2f87a3774d3cd6cb7d3cdbba393e4d2c2222
Daniel Korosi <daniel.korosi42@gmail.com>
2017-07-10 09:21:42 +0200

    ex4 done

---
+++ final-exam/ex4.js
@@ -0,0 +1,11 @@
+//Create a function that returns how many letters in the word: "duck" (it is four)
+
+function letterCount(word) {
+  let count = 0;
+  for (let i = 0; i < word.length; i++) {
+    count++
+  }
+  return count
+}
+
+console.log(letterCount('duck'));
